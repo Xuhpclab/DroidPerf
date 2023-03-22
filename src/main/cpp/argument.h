b6989e7484fa9cdabdd5b60fb90b6152bf333adb
MinghoKwok <995666769@qq.com>
2023-03-22 17:29:50 -0400

    Recover .cpp

---
+++ src/main/cpp/argument.h
@@ -0,0 +1,25 @@
+//
+// Created by Bolun Li on 9/21/21.
+//
+
+#ifndef _ARGUMENT_H
+#define _ARGUMENT_H
+
+#include <vector>
+#include <string>
+
+class Argument {
+public:
+  Argument(const char* arg);
+  ~Argument();
+
+  const std::vector<std::string> &getPerfEventList(){ return _perf_event_list;};
+
+private:
+
+  //std::vector<std::string> _event_list; // string format: EVENT_NAME@SAMPLING_RATE
+  std::vector<std::string> _perf_event_list;
+
+};
+
+#endif //_ARGUMENT_H
